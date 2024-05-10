0456a8355412a664ec71925fea15a11d111175bf
kimbro97 <rlagudwog@gmail.com>
2024-05-10 18:06:38 +0900

    feat: 제네릭 메서드 활용

---
+++ src/generic/ex4/AnimalMethod.java
@@ -0,0 +1,15 @@
+package generic.ex4;
+
+import generic.animal.Animal;
+
+public class AnimalMethod {
+    public static <T extends Animal> void checkup(T animal) {
+        System.out.println("동물 이름: " + animal.getName());
+        System.out.println("동물 크기: " + animal.getSize());
+        animal.sound();
+    }
+
+    public static <T extends Animal> T bigger(T target1, T target2) {
+        return target1.getSize() > target2.getSize() ? target1 : target2;
+    }
+}
